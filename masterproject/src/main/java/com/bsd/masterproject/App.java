4d44c692363d322bd62bb7776e9ab94b4130312b
yui <yui018@hotmail.com>
2018-05-20 18:07:05 +0700

    first commit

---
+++ masterproject/src/main/java/com/bsd/masterproject/App.java
@@ -0,0 +1,13 @@
+package com.bsd.masterproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
